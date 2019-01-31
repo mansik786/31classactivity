a6112b1a709773ad36391b799a04e2517c6ab292
visualstudio <visualstudio@230-PC21>
2019-01-31 12:01:24 -0500

    //740950

--- 31_1_19/js/main.js
+++ 31_1_19/js/main.js
@@ -1,1 +1,6 @@
 ﻿// Your code here!
+function myFunction() {
+
+    var cat = new Date();
+    document.body.innerHTML = "<h1> the date is " + (cat.getMonth() + 1) + "/" + cat.getDate() + "/" + cat.getFullYear() + "<h1>" 
+}
